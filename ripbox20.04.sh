4f5480e8f9b0b3ac88ac6436d6ebc9a8f22217b8
jack2game <rex.txt@gmail.com>
2020-06-23 23:40:50 +0800

    Update ripbox20.04.sh

--- ripbox20.04.sh
+++ ripbox20.04.sh
@@ -4,9 +4,6 @@ sudo apt-get -y install software-properties-common
 sudo apt-get -y install nano
 sudo apt update
 sudo apt -y install xfce4 xfce4-goodies
-31
-1
-1
 sudo apt -y install tightvncserver
 touch ~/.Xresources
 vncserver -kill :1
