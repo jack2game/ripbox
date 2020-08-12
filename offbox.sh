50dff46e75cc663002e60fb45d8cfd586ce9dc29
jack2game <rex.txt@gmail.com>
2020-08-12 23:45:35 +0800

    Create offbox.sh

---
+++ offbox.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sleep 30720s #750GB@25MB/s
+export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
+export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
+gcloud --quiet compute instances delete $NAME --zone=$ZONE
