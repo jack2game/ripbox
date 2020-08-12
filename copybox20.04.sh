df30ae3367a56a274f2d4e22e76c983a4835788c
jack2game <rex.txt@gmail.com>
2020-08-12 21:00:20 +0800

    Create copybox20.04.sh

---
+++ copybox20.04.sh
@@ -0,0 +1,4 @@
+sudo curl https://rclone.org/install.sh | sudo bash
+mkdir /home/rex_txt/.config/rclone/
+cp /home/rex_txt/rclone.conf /home/rex_txt/.config/rclone/
+rclone copy remote0_crypt:/ imperial_crypt:/ --verbose
