aa0b2657274e062a9a3b3236855e8a79d96d6018
jack2game <rex.txt@gmail.com>
2020-08-12 21:07:08 +0800

    Update copybox20.04.sh

--- copybox20.04.sh
+++ copybox20.04.sh
@@ -1,3 +1,5 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo curl https://rclone.org/install.sh | sudo bash
 mkdir /home/rex_txt/.config/rclone/
 cp /home/rex_txt/rclone.conf /home/rex_txt/.config/rclone/
